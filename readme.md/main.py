8b912c9c2d595ff4e35e7ff465abc482d195ccf0
Cborraga <corwin0934@gmail.com>
2021-02-22 16:24:04 -0500

    Startup for my short based text adventure game!

---
+++ readme.md/main.py
@@ -0,0 +1,16 @@
+import time
+
+answer_A = ["A", "a", "1"]
+answer_b = ["B", "b", "2"]
+answer_c = ["C", "c", "3"]
+yes = ["Yes", "yes", "y", "Y"]
+no = ["No", "no", "n", "N"]
+
+gold = 0
+
+While True:
+
+required = ("/nPlease only use only A, B, C, or numbers.")
+
+#might not need this type of code... gender = input ("Are you male or female?")
+#story
